3e5a932761cd4360797dc821d50ba1400531e8cc
Sem <semik.kolesnikovv@gmail.com>
2025-02-06 18:29:33 +0100

    Initial Commit

---
+++ Translator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Translator
+//
+//  Created by Sem Koliesnikov on 06/02/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
